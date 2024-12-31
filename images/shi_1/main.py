5fc7f4f57aa790b191f8c269813816826dccfeb0
adityaksoni234 <adityaksoni234@gmail.com>
2024-12-31 12:30:48 +0530

    first commit

---
+++ images/shi_1/main.py
@@ -0,0 +1,31 @@
+def ml_output(self, Landslides,Earthquakes,Volcanic_eruptions,Tornadoes,Floods,Tsunamis,Droughts,Wildfires,Storm):
+
+
+    if ml_output() == Earthquakes:
+        print("Earthquakes detected in the region ")
+
+    elif ml_output() == Volcanic_eruptions:
+        print("Volcanic eruptions detected in the region")
+
+        print("Tornadoes detected in the region")
+
+    elif ml_output() == Floods:
+        print("Floods detected in the region")
+
+    elif ml_output() == Landslides:
+        print("Landslides detected in the region")
+
+    elif ml_output() == Tsunamis:
+        print("Tsunamis detected in the region")
+
+    elif ml_output() == Droughts:
+        print("Droughts detected in the region")
+
+    elif ml_output() == Wildfires:
+        print("Wildfires detected in the region")
+
+    elif ml_output() == Storm:
+        print("Storm detected in the region")
+
+    else:
+        print("SORRY THIS DESISTER HAS NOT BEEN DETECTED ")
